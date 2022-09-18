cb8943483a272fdd7002476a0c818d89904d2edd
Faitsumaru <kgm20011505@mail.ru>
2022-09-18 18:25:41 +0300

    site info modifications v.2 (cv changing)

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-//import CV from '../../assets/cv.pdf'
+import CV from '../../assets/cv.pdf'
 
 const CTA = () => { //header buttons
   return (
     <div className="cta">
-        <a href={'https://sweetcv.com/lx-aoil6v2l_b'} download target='_blank' className='btn'>Open CV</a>
+        {/* <a href={'https://sweetcv.com/lx-aoil6v2l_b'} download target='_blank' className='btn'>Open CV</a> */}
+        <a href={CV} target='_blank' className='btn'>Open CV</a>
         <a href="#contact" className='btn btn-primary'>Let's talk</a>
     </div>
   )
